0611ec79dc586b4d9870c77f043fe884a4fa54b8
Nolife <69518147+krkkrk11@users.noreply.github.com>
2022-11-19 14:47:18 +0700

    Update script_BokuNoRoblox.lua

--- script_BokuNoRoblox.lua
+++ script_BokuNoRoblox.lua
@@ -1,42 +1,11 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("NoName Hub", "DarkTheme")
-local Tab = Window:NewTab("Main")
-local Section = Tab:NewSection("Auto farm")
-
+--All local to farm
 local ws = game:GetService("Workspace")
-local player = game:GetService("Workspace").krkkrk112233
 local a1 = ws.NPCs["Awakened Tomura"].HumanoidRootPart.CFrame
 local hv = CFrame.new(0,96,0)
 local MONlist = nil
 local Weaponlist = nil
 
-
-
-for i,v in pairs(game:GetService("Players").krkkrk112233.PlayerGui.MainMenus.WeaponsMenu.ScrollingFrame:GetChildren()) do
-    table.insert(Weaponlist,v.Name)
-end
-
-
-
-Section:NewDropdown("select weapon", " ", Weaponlist, function(wp)
-    Weapon = wp
-end)
-
-
-
-
-Section:NewToggle("Autofarm", "ToggleInfo", function(state)
-_G.tp = state
-end)
-
-
-function equip()
-    local args = {
-        [1] = Weapon
-    }
-    
-    game:GetService("ReplicatedStorage").WeaponShop.Equip:FireServer(unpack(args))
-end
+--All function
 
 function hitmon()
     wait(2)
@@ -45,27 +14,57 @@ function hitmon()
     }
     
     game:GetService("Players").LocalPlayer.Character.Main.Swing:FireServer(unpack(args))
+    wait(1.6)
+    local args2 = {
+        [1] = a1 * CFrame.Angles(0,0,0)
+    }
+    game:GetService("Players").LocalPlayer.Character.Main.RightSwing:FireServer(unpack(args2))
+end
+function tptomon()
+    local TweenService = game:GetService("TweenService")
+    local Tw = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(0, Enum.EasingStyle.Linear, Enum.EasingDirection.Out,0,false,0), 
+    {CFrame = a1*hv}):Play()
 end
 
 
 
-spawn(function()
-    while wait() do
-        if _G.tp then
-            player.HumanoidRootPart.CFrame = a1 * hv
-            game.workspace.Gravity = 0
-            hitmon()
-        else
-            game.workspace.Gravity = 196
-        end
-    end
+
+--ui Library
+ 
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Nolife Hub", "DarkTheme")
+local Tab = Window:NewTab("Main")
+local Section = Tab:NewSection("Auto farm")
+Section:NewToggle("Autofarm", "ToggleInfo", function(state)
+_G.Autofarm = state
 end)
+Section:NewButton("Equip gun", "ButtonInfo", function()
+    local args = {
+    [1] = "OrnamentBlaster"
+    }
+    
+    game:GetService("ReplicatedStorage").WeaponShop.Equip:FireServer(unpack(args))
+end)
+local Tab = Window:NewTab("Stats")
+local Section = Tab:NewSection("Auto up Stats")
+Section:NewDropdown("Stats", "Stat", {"Strength", "Agility", "Durability"}, function(sadw)
 
-
+end)
+Section:NewToggle("Autoupstat", "ToggleInfo", function(stat)
+_G.Autoupstat = stat
+end)
+local Tab = Window:NewTab("Teleport")
+local Section = Tab:NewSection("Teleport to mon")
+Section:NewButton("Tp Awakened Tomura", "ButtonInfo", function()
+    local TweenService = game:GetService("TweenService")
+    local Tw = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(0, Enum.EasingStyle.Linear, Enum.EasingDirection.Out,0,false,0), 
+    {CFrame = a1}):Play()
+end)
 local Tab = Window:NewTab("setting")
 local Section = Tab:NewSection("function rejoin")
-Section:NewToggle("rejoin", "ToggleInfo", function(vs)
-_G.Rejoin = vs
+Section:NewToggle("rejoin", "ToggleInfo", function(rj)
+_G.Rejoin = rj
 spawn(function()
     while true do wait()
         getgenv().rejoin = game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(Kick)
@@ -80,6 +79,67 @@ spawn(function()
 end)
 end)
 local Section = Tab:NewSection("Hide Ui")
-Section:NewKeybind("KeybindText", "KeybindInfo", Enum.KeyCode.RightControl, function()
+Section:NewKeybind("KeybindText", "KeybindInfo", Enum.KeyCode.RightShift, function()
 	Library:ToggleUI()
+end)
+
+
+
+
+
+
+
+
+
+--Auto All
+
+
+spawn(function()
+    while wait() do
+        if _G.Autoupstat then
+            if sadw == Strength then
+                local args = {
+                    [1] = 1
+                }
+                
+                game:GetService("ReplicatedStorage").Remotes.Strength:FireServer(unpack(args))
+            elseif Stats == Agility then
+                local args = {
+                    [1] = 1
+                }
+                
+                game:GetService("ReplicatedStorage").Remotes.Agility:FireServer(unpack(args))
+            elseif Stats == Durability then
+                local args = {
+                    [1] = 1
+                }
+                
+                game:GetService("ReplicatedStorage").Remotes.Durability:FireServer(unpack(args))
+            end
+        end
+    end
+end)
+spawn(function()
+    while wait() do
+        if _G.Autofarm then
+            hitmon()
+        end
+    end
+end)
+spawn(function()
+    while wait() do
+        if not game:GetService("Players").krkkrk112233.PlayerGui.MainMenus.Inventory.CurrentWepImage.image == 6120801606 then
+            local args = {
+                [1] = "OrnamentBlaster"
+            }
+            
+            game:GetService("ReplicatedStorage").WeaponShop.Equip:FireServer(unpack(args))
+        end
+        if _G.Autofarm then
+            tptomon()
+            game.workspace.Gravity = 0
+        else
+            game.workspace.Gravity = 196
+        end
+    end
 end)
